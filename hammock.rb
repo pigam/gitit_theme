d15d5ca2a52465b3ab9917c30cb8a7b1ae0a160e
Adam Watkins <adam@stupidpupil.co.uk>
2017-01-09 18:06:33 +0000

    Expand the hammock

--- hammock.rb
+++ hammock.rb
@@ -7,6 +7,28 @@ get '/' do
   page.gsub!('$base$','')
   page.gsub!('$pagetitle$', 'Gitit\'s User Guide')
   page.gsub!('$wikititle$', 'Not a Real Wiki')
+  page.gsub!('$pageUrl$', '/')
   page.gsub!('$content()$', File.read(File.dirname(__FILE__)+'/fixtures/view.html'))
+
+  page
+end
+
+get '/_edit/' do
+  page = File.read(File.dirname(__FILE__) + '/templates/page.st')
+  page.gsub!('$base$','')
+  page.gsub!('$pagetitle$', 'Gitit\'s User Guide')
+  page.gsub!('$wikititle$', 'Not a Real Wiki')
+  page.gsub!('$pageUrl$', '')
+  page.gsub!('$content()$', File.read(File.dirname(__FILE__)+'/fixtures/edit.html'))
+  page
+end
+
+get '/_index' do
+  page = File.read(File.dirname(__FILE__) + '/templates/page.st')
+  page.gsub!('$base$','')
+  page.gsub!('$pagetitle$', 'Gitit\'s User Guide')
+  page.gsub!('$wikititle$', 'Not a Real Wiki')
+  page.gsub!('$pageUrl$', '')
+  page.gsub!('$content()$', File.read(File.dirname(__FILE__)+'/fixtures/index.html'))
   page
 end
